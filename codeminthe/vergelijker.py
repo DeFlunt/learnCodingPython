85be580789c33e5b05c25448edad8db2c845b6b6
Rutger Le Jeune <rutger-lejeune@hotmail.com>
2020-10-05 21:51:05 +0200

    Made code accourding to 3.8 iso 2.x

--- codeminthe/vergelijker.py
+++ codeminthe/vergelijker.py
@@ -1,12 +1,13 @@
 print("Hallo, we gaan samen wat getallen vergelijken")
 
-refGetal = int(raw_input("Geef een referentie getal: "))
+refGetal = input("Geef een referentie getal: ")
+refGetal = int(refGetal)
 
 teller = 1
 aantal = 7
 
 while teller <= aantal:
-    antwoord = int(raw_input("Geef een getal: "))
+    antwoord = int(input("Geef een getal: "))
 
     if antwoord < refGetal:
         print("\t " + str(antwoord) + " is kleiner dan " + str(refGetal))
@@ -14,5 +15,6 @@ while teller <= aantal:
         print("\t " + str(antwoord) + " is gelijk aan " + str(refGetal))
     elif antwoord > refGetal:
         print("\t " + str(antwoord) + " is groter dan " + str(refGetal))
+    teller += 1
 
 print("EINDE")
